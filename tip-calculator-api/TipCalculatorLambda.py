d717b0c5eaf443c038930c3d174350e8cacbabad
Kwan-yuet Ho <stephenhky@yahoo.com.hk>
2020-07-29 23:45:10 -0400

    deployed

--- tip-calculator-api/TipCalculatorLambda.py
+++ tip-calculator-api/TipCalculatorLambda.py
@@ -1,4 +1,6 @@
 
+import json
+
 
 taxrates = {'MD': 0.06, 'VA': 0.06, 'DC': 0.10}
 
@@ -17,10 +19,23 @@ def calculate_tips(subtotal, state, tippercent=0.15, split=1):
 
 
 def lambda_handler(event, context):
-    subtotal = event['subtotal']
-    state = event['state']
-    tippercent = event.get('tippercent', 0.15)
-    split = event.get('split', 1)
+    query = json.loads(event['body'])
+    subtotal = query['subtotal']
+    state = query['state']
+    tippercent = query.get('tippercent', 0.15)
+    split = query.get('split', 1)
 
     calculation_result = calculate_tips(subtotal, state, tippercent=tippercent, split=split)
-    return calculation_result
+    event['result'] = json.dumps(calculation_result)
+    req_res = {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps(event)
+    }
+    return req_res
+
+
+# curl command to run this:
+# curl --location --request GET 'https://1j79chd1w8.execute-api.us-east-1.amazonaws.com/default/Ingram1623TipCalculator' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{"subtotal": 100, "state": "MD", "split": 2}'
