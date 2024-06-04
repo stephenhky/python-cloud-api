b7d4a5de1ea9d3a80f76d678e83cb6fd3a19f9e2
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2024-06-03 21:51:29 -0400

    removed langchain components

--- aws-bedrock-llm-api/bedrockutils/bedrockllm.py
+++ aws-bedrock-llm-api/bedrockutils/bedrockllm.py
@@ -1,15 +1,6 @@
 
 import json
 
-import boto3
-from langchain.llms.bedrock import Bedrock
-from langchain.chains import LLMChain
-from botocore.config import Config
-
-
-
-get_bedrock_runtime = lambda region_name: boto3.client(service_name='bedrock-runtime', region_name=region_name)
-
 
 def call_bedrock_models(prompt_config, model_id, bedrock_runtime):
     body = json.dumps(prompt_config)
@@ -29,5 +20,4 @@ def call_bedrock_models(prompt_config, model_id, bedrock_runtime):
     return results
 
 
-def get_langchain_bedrock_llm(model_id, client, *args, **kwargs):
-    return Bedrock(model_id=model_id, client=client, *args, **kwargs)
+
